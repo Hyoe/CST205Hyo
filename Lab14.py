83bd81f165d6d6cc5bfa19a5f49efda5458e07bb
Hyoe <hyoyoe@gmail.com>
2014-12-08 17:25:27 -0800

    First and Second part of Lab 14

---
+++ Lab14.py
@@ -0,0 +1,37 @@
+# Hyo Lee
+# CST 205
+# Lab 14 - Files in Python
+# 12/08/2014
+
+# Problem 1:
+
+#
+def totalWords():
+  #Open eggs.txt
+  file = "C:\Users\Me\Documents\CST205\CST205\Functions\Lab14\eggs.txt"
+  openFile = open(file, "rt")
+
+  wordCount = 0   
+  
+  for lines in openFile:
+    wordsNoSpaces = lines.split()  #Stores words in lists where default delimiter is blank space with built-in string method split(), resource: https://docs.python.org/2/library/stdtypes.html
+    wordCount = wordCount + len(wordsNoSpaces)
+  print "Total word count = ", str(wordCount)
+  openFile.close()
+   
+
+def sameWords():
+  file = "C:\Users\Me\Documents\CST205\CST205\Functions\Lab14\eggs.txt"
+  openFile = open(file, "rt")
+  text = openFile.read()
+  wordsUpper = text.upper()
+  words = wordsUpper.split()
+  counts = {}
+  for word in words:
+    if word in counts:
+      counts[word] = counts[word] + 1
+    else:
+      counts[word] = 1
+  print counts
+  
+  
